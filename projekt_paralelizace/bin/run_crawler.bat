fdae8563e1ac12358e734e21c03c164ce2e27543
mensik <mensik.tony@gmail.com>
2025-12-10 19:40:56 +0100

    deleted whole project

--- projekt_paralelizace/bin/run_crawler.bat
+++
@@ -1,7 +0,0 @@
-:: Author: Tony Mensik
-:: Script: run_crawler.bat
-:: Description: Starts the parallel web crawler using Python from the /src directory.
-
-echo Starting parallel web crawler...
-py ..\src\main.py
-pause
